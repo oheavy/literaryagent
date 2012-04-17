e147910f479bfb41536894c9d9b88632b84123a3
Luis Vieira <luis.vieira.1985@gmail.com>
2012-04-17 01:19:08 +0100

    restart

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Literaryagent::Application.config.session_store :cookie_store, key: '_literaryagent_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Literaryagent::Application.config.session_store :active_record_store
