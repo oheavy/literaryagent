048f3a58acca833d2e0dc642348d214acfb4b637
Luis Vieira <luis.vieira.1985@gmail.com>
2012-04-17 01:21:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Literaryagent::Application.config.secret_token = '4b0d81787b12a8c3d471d7ff95088754dd070ceb5499cfabe0456c188476abf34fabe2cb4c94bf4ecc5810743b0831ef60fa121d7e1d7ba40ae9c04b8eb4e996'
